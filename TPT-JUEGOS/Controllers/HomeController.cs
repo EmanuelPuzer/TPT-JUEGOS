89a26082fcea56b8b102515b9d42d38af3541967
EmanuelPuzer <emanuelgpuzer@hotmail.com>
2025-11-10 23:33:08 -0300

    inicio sesion post charla con profesor

--- TPT-JUEGOS/Controllers/HomeController.cs
+++ TPT-JUEGOS/Controllers/HomeController.cs
@@ -1,20 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using TPT_JUEGOS.Models;
-using TPT_JUEGOS.Context;
-using Microsoft.EntityFrameworkCore;
 
 namespace TPT_JUEGOS.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly UsuariosDatabaseContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, UsuariosDatabaseContext context)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _context = context;
         }
 
         public IActionResult Index()
@@ -22,39 +18,10 @@ namespace TPT_JUEGOS.Controllers
             return View();
         }
 
-        [HttpGet]
         public IActionResult InicioSesion()
         {
-            return View(new LoginViewModel());
+            return View();
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> InicioSesion(LoginViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                var inputUsuario = viewModel.UsuarioOEmail;
-
-                var usuarioEncontrado = await _context.Usuarios
-                    .FirstOrDefaultAsync(u =>
-                        (u.CORREO_USUARIO == inputUsuario || u.NOMBRE_USUARIO == inputUsuario) &&
-                         u.CONTRASENA_USUARIO == viewModel.Contrasena);
-
-                if (usuarioEncontrado == null)
-                {
-                    ModelState.AddModelError(string.Empty, "Usuario, correo o contraseña incorrectos.");
-                    return View(viewModel);
-                }
-
-                
-
-                return RedirectToAction("Index", "Home");
-            }
-
-            return View(viewModel);
-        }
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -63,3 +30,4 @@ namespace TPT_JUEGOS.Controllers
         }
     }
 }
+
